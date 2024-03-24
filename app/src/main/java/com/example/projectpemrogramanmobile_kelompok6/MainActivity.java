6850d148563067073b47158b9bc090b0fb19f015
gabrieeelms498 <gabrieeelms498@gmail.com>
2024-03-24 20:54:06 +0700

    tes

---
+++ app/src/main/java/com/example/projectpemrogramanmobile_kelompok6/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.projectpemrogramanmobile_kelompok6;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+import android.window.SplashScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    private int waktu_loading=4000;
+    //4000=4 detik
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home=new Intent(MainActivity.this, SplashScreen.class);
+                startActivity(home);
+                finish();
+            }
+        },waktu_loading);
+    }
+}
---
+++ app/src/main/java/com/example/projectpemrogramanmobile_kelompok6/splashscreen.java
@@ -0,0 +1,14 @@
+package com.example.projectpemrogramanmobile_kelompok6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
